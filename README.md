165228f2a148ff42672a0d5e174cddba8828580f
Dmitriy Bodan <dmitriybodan@gmail.com>
2015-06-08 22:06:00 -0400

    v.0,95

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # project_1
 tic_tac_toe
+
+Mortal Kombat tic tac toe.
+
+Presented by the outstanding characters of Midway, two immortal rivals in the face of Scorpion and Sub-Zero.
+
+Basic knowledge of html, css, JavaScript and jQuery.
+
+DOM manipulations and a lot arrays are in here.
+
+Everything is working as a part of one big object, comparing the <tds> we are comparing the real “what’s inside” of each of them.
+
+Used conditions and loops to get it all together.
+
+Needs to be added more of styles. Sounds and animations to make the experience as much entertaining as we can while playing TTT, fatalities, brutalities and old school rocking action/fighting universe will help the boring and logic TTT look like an awesome time burner for all ages.
+
+Different characters and outcomes is on its way.
+
+Board 4x4 and 5x5 planned to be present so the fun from the logical fighting between immortal characters got you super excited. AI is under constraction for now, but main algorithms already read and being planned right now to implement asap. 
--- tic_tac_toe/project1.js
+++ tic_tac_toe/project1.js
@@ -1,67 +1,149 @@
 board = {
 
-playerArr : [1,2],
+  playerArr: [1, 2],
 
-boardArr : [[],[]],
+  boardArr: [
+    [],
+    [],
+    []
+  ],
 
-switchPlayer : function () {  /* switching playas */
-	this.playerArr.push(this.playerArr.shift());
-},
+  switchPlayer: function() { /* switching playas */
+    this.playerArr.push(this.playerArr.shift());
+  },
 
-horizontalCheck : function (x){   /* checking horizontal squares for win */
-	for (var i=0; i<this.boardArr.length;i++) {
-		if (this.boardArr[x][0]!==this.boardArr[x][i]) {
-			return false;
-		}
-	} return true;
-},
-
-
-verticalCheck : function (x){ /* checks vertical */
-	for (var i=0; i<this.boardArr.length;i++) {
-		if (this.boardArr[x][0]!==this.boardArr[i][x]) {
-			return false;
-			}
-	} return true;
-},
-
-diagLCheck : function (){ /* checks diagonal Left */
-	for (var i=0; i<this.boardArr.length;i++) {
-		if (this.boardArr[0][0]!==this.boardArr[i][i]) {
-			return false;
-		}
-	} return true;
-},
+  horizontalCheck: function(row) { /* checking horizontal squares for win */
+    for (var i = 0; i < this.boardArr.length; i++) {
+      if (this.boardArr[row][0].innerHTML !== this.boardArr[row][i].innerHTML || this.boardArr[row][0].innerHTML == "") {
+        return false;
+      }
+    }
+    return true;
+  },
 
 
-diagRcheck : function (){  /* checks diagonal Right */
-	for (var i=0; i<this.boardArr.length;i++) {
-		if (this.boardArr[0][x]!==this.boardArr[i][x-i]){
-	 		return false;
-		}
-	} return true;
-},
+  verticalCheck: function(column) { /* checks vertical */
+    for (var i = 0; i < this.boardArr.length; i++) {
+      if (this.boardArr[0][column].innerHTML !== this.boardArr[i][column].innerHTML || this.boardArr[0][column].innerHTML == "") {
+        return false;
+      }
+    }
+    return true;
+  },
 
-boardInfo : [],
+  diagLCheck: function() { /* checks diagonal Left */
+    for (var i = 0; i < this.boardArr.length; i++) {
+      if (this.boardArr[0][0].innerHTML !== this.boardArr[i][i].innerHTML || this.boardArr[0][0].innerHTML == "") {
+        return false;
+      }
+    }
+    return true;
+  },
 
-setEventListener : function () {
-	var knopki = document.querySelectorAll('td');
-	for (var i=0;i<knopki.length;i++) {
 
-		
-		knopki[i].addEventListener('click', function (event) {
-			currentThis = this;
-			if (currentThis.innerHTML==="") {
-				if (board.playerArr[0]===1) {
-					currentThis.innerHTML = ("<img src='http://vignette1.wikia.nocookie.net/mkwikia/images/f/f3/Scorpion-mk2-fix1.gif/revision/latest?cb=20081004200429'/>");
-				}
-				else if (board.playerArr[0]===2) {
-					currentThis.innerHTML = ("<img src='http://vignette3.wikia.nocookie.net/mkwikia/images/3/3c/Subzero-mk2-fix.gif/revision/latest?cb=20081004200559'/>");
-				}
-				board.switchPlayer();
-				board.boardInfo.push(knopki[i]);
-			}
-		})
-	}
+  diagRCheck: function(row) { /* checks diagonal Right */
+    for (var i = 0; i < this.boardArr.length; i++) {
+      if (this.boardArr[0][row].innerHTML !== this.boardArr[i][row - i].innerHTML || this.boardArr[0][row].innerHTML == "") {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  boardInfo: [],
+
+  setEventListener: function() {
+    var knopki = document.querySelectorAll('td');
+    for (var i = 0; i < knopki.length; i++) {
+      this.boardInfo.push(knopki[i])
+
+      for (var k = 0; k < 3; k++) {
+        for (var j = 0; j < 3; j++) {
+          board.boardArr[k][j] = (board.boardInfo[k * 3 + j]);
+        }
+      }
+      board.boardInfo[i].addEventListener('click', function(event) {
+        currentThis = this;
+        if (currentThis.innerHTML === "") {
+          if (board.playerArr[0] === 1) {
+            currentThis.innerHTML = ("<img src='http://vignette1.wikia.nocookie.net/mkwikia/images/f/f3/Scorpion-mk2-fix1.gif/revision/latest?cb=20081004200429'/>");
+          } else if (board.playerArr[0] === 2) {
+            currentThis.innerHTML = ("<img src='http://vignette3.wikia.nocookie.net/mkwikia/images/3/3c/Subzero-mk2-fix.gif/revision/latest?cb=20081004200559'/>");
+          }
+          board.switchPlayer();
+          board.winnerCheck();
+        }
+      })
+    }
+  },
+
+  // Check to see who has won
+  winnerCheck: function() {
+    for (var i = 0; i < 3; i++) {
+      var x = this.horizontalCheck(i);
+      var y = this.verticalCheck(i);
+      if (x) {
+        alert("Flawless Victory");
+      }
+      if (y) {
+        alert("Victory");
+      }
+      if (this.diagLCheck()) {
+        alert("Left Cross Victory");
+      }
+      if (this.diagRCheck(i)) {
+        alert("Right Cross Victory");
+      }
+    }
+    return null;
+  },
+
+  // check to see if there is tie
+  tieCheck: function() {
+    if (this.horizontalCheck() !== true || this.verticalCheck() !== true || this.diagLCheck() !== true || this.diagRCheck() !== true) {
+      return "Tie";
+    }
+  },
+  //reseting everything
+  reset: function() {
+    var $td = $('td');
+    for (var i = 0; i < 9; i++) {
+      $($td[i]).html("");
+    }
+    this.boardArr = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""]
+    ];
+    board.boardInfo = [];
+    board.playerArr = [1, 2];
+    board.setEventListener();
+  },
+
+
 }
-}
+$("#button").on("click", board.reset);
+
+//to enter their own name for the game
+// enterNames= function() {
+
+//   var self = this;
+//   var nameCounter = 0;  //to store names properly
+
+//   $('input').attr('placeholder', "Please Enter Player One's Name")
+//             .on('keydown', function (e) {
+//               if(e.keyCode === 13 && nameCounter === 0) {
+//                 self.playerOneName = $('input').val();
+//                 $('input').val('')
+//                           .attr('placeholder', "Please Enter Player Two's Name");
+//                 nameCounter++;
+//               } else if(e.keyCode === 13 && nameCounter === 1) {
+//                 self.playerTwoName = $('input').val();
+//                 $('input').remove();
+//               }
+//             });
+// },
+
+
+board.setEventListener();
+board.reset();
